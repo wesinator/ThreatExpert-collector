f9c6b97445fc2c19a2678d26ce35644d080e1d04
Wesinator <5124946+wesinator@users.noreply.github.com>
2018-04-16 17:29:46 -0400

    Correct headers arg

--- threatexpert_collector.py
+++ threatexpert_collector.py
@@ -26,20 +26,20 @@ def download(conn_obj, path, out_file):
 	# Add slash to beginning of path if not already present
 	if path[0] != '/':
 		path = '/' + path
-	
+
 	try:
 		conn_obj.request("GET", path, headers=req_headers)
 		res = conn_obj.getresponse()
-		
+
 		if res.status == 200:
 			data = res.read()
 			with open(out_file, "wb") as f:
 				f.write(data)
 			return data
-				
+
 		else:
 			print("Could not get %s\nStatus code: %d" % (path, res.status))
-	
+
 	except Exception as e:
 		print("An error occurred downloading %s\n%s" % (path, str(e)))
 
@@ -47,34 +47,34 @@ def download(conn_obj, path, out_file):
 def get_te_reports(reports_html, reports_dir):
 	new_report_count = 0 # Counter for showing how many new reports are found
 	parse = BeautifulSoup(reports_html)
-	
+
 	for report in parse.find_all('a', attrs = {"href": re.compile(md5_re)}):
 		report_path = report.get('href')
 		md5 = report_path[16:] # Get MD5 from report path, starts at index 16
-		
+
 		# If the report file for an md5 does not already exist, download the report
 		if not os.path.isfile("%s%s.html" % (reports_dir, md5)):
 				# Create reports dir if it doesn't already exist
 				#if not os.path.exists(reports_dir):
 					#os.mkdir(reports_dir)
-				
+
 				# Append the public URL to the url file list:
 				#with open("urls.txt", "a") as url_list:
 					#url_list.write("\nhttp://www.threatexpert.com/" + report_path)
-				
+
 				new_report_count += 1 # Increment new report count
 				print("Retrieving report for MD5: %s" % md5)
 				report = download(conn, report_path, "%s%s.html" % (reports_dir, md5))
-				
+
 				# If report has image, get the image
 				if "The new window was created, as shown below:" in report:
 					parse_img = BeautifulSoup(report)
 					for img in parse_img.find_all("img", attrs = {"src": re.compile(img_re)}):
 						img_path = img.get("src")
-						
+
 						print("Downloading image for report " + md5)
 						img_data = download(conn, img_path, "%s%s.gif" % (reports_dir, md5))
-	
+
 	print("%d new reports processed." % new_report_count)
 	return new_report_count
 
@@ -83,14 +83,14 @@ def get_te_latest_reports():
 	for i in range(1, 11):
 		# Build request for reports.aspx page
 		print("\nRetrieving reports page %d\n" % i)
-		conn.request("GET", "/reports.aspx?page=%d" % i, None, headers)
+		conn.request("GET", "/reports.aspx?page=%d" % i, None, req_headers)
 		res = conn.getresponse()
-		
+
 		# Call reports parser , show number of new reports processed.
 		if res.status == 200:
 			reports = res.read()
 			count = get_te_reports(reports, "reports/")
-			
+
 			# End the loop if no new reports are found
 			if count == 0:
 				print("Stopping since no new reports were found on the last page.")
@@ -100,17 +100,17 @@ def get_te_latest_reports():
 
 def get_te_search_results(search_term):
 	query = search_term.replace(' ', '+')
-	
+
 	for i in range(1, 11):
 		search_path = "/reports.aspx?page=%d&find=%s" % (i, query)
 		print("\nRetrieving results for '%s' page %d:" % (search_term, i))
-		conn.request("GET", search_path, None, headers)
+		conn.request("GET", search_path, None, req_headers)
 		res = conn.getresponse()
-		
+
 		if res.status == 200:
 			results = res.read()
 			get_te_reports(results, "reports/")
-			
+
 			# Stop if the search doesn't have any more result pages
 			if "reports.aspx?page=%d&find=%s" % (i + 1, query.replace('~', "%7E")) not in results:
 				print("\nNo more results pages are available.")
@@ -120,12 +120,12 @@ def main():
 	# If no search query , go through the latest 10 report pages.
 	if len(sys.argv) == 1:
 		get_te_latest_reports()
-	
+
 	# If the user provided a search query , return the matching reports for the query and download them if they don't already exist.
 	elif len(sys.argv) == 2:
 		search = sys.argv[1].strip('"')
 		get_te_search_results(search)
-	
+
 	# If user provided more than one arg , print usage message
 	else:
 		print("Error: This program can only take none or one user provided argument.\nIf you want to search for a multiword phrase, please surround it in quotes.")
