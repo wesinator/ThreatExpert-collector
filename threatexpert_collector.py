8ac88821f4aa413e815c5e1aec9114cb76ad5794
wesinator <5124946+wesinator@users.noreply.github.com>
2018-05-26 14:02:05 -0400

    Working search query results

--- threatexpert_collector.py
+++ threatexpert_collector.py
@@ -2,30 +2,28 @@
 from bs4 import BeautifulSoup
 import os
 from random import randint
-from time import sleep
 import re
 import requests
+import sys
+from time import sleep
+from urllib.parse import quote_plus
 
 reports_dir = "ThreatExpert reports"
 te_md5s_list = "threatexpert_md5s.txt"
 
 ie11_req_headers = {
-	"Accept": "text/html, application/xhtml+xml, */*",
-	"Accept-Language": "en-US",
-	"User-Agent": "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; .NET4.0E; .NET4.0C; rv:11.0) like Gecko",
-	"Accept-Encoding": "gzip, deflate",
-	"DNT": "1",
-	"Connection": "keep-alive"
+    "Accept": "text/html, application/xhtml+xml, */*",
+    "Accept-Language": "en-US",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; .NET4.0E; .NET4.0C; rv:11.0) like Gecko",
+    "Accept-Encoding": "gzip, deflate",
+    "DNT": "1",
+    "Connection": "keep-alive"
 }
 
 # Get MD5s on a report page
-def get_report_md5s(page_number):
-    te_reports_url = "http://www.threatexpert.com/reports.aspx?page=%d" % page_number
-
+def get_report_md5s(reports_html):
     md5s = []
-
-    req = requests.get(te_reports_url, headers=ie11_req_headers)
-    soup = BeautifulSoup(req.text, "lxml")
+    soup = BeautifulSoup(reports_html, "lxml")
 
     for report in soup.find_all('a', href=True, target="_blank"):
         md5 = report["href"].replace("report.aspx?md5=", '')
@@ -72,6 +70,21 @@ def get_te_screenshot(report_html, md5):
         with open("%s/%s.gif" % (reports_dir, md5), 'wb') as img_file:
             img_file.write(r.raw.read())
 
+def get_te_search_results(search_term):
+    query = quote_plus(search_term)
+
+    for i in range(1, 11):
+        search_url = "http://www.threatexpert.com/reports.aspx?page=%d&find=%s" % (i, query)
+        print("\nRetrieving results for '%s' page %d:" % (search_term, i))
+        req = requests.get(search_url, headers=ie11_req_headers)
+
+        get_report_md5s(req.text)
+
+        # Stop if the search doesn't have any more result pages
+        if "reports.aspx?page=%d&find=%s" % (i + 1, query) not in req.text:
+            print("\nNo more results pages are available.")
+            break
+
 # Get MD5s and download reports for pages 1-10
 def main():
     # Create reports dir if it doesn't already exist
@@ -81,11 +94,21 @@ def main():
     md5s = []
 
     md5_list = open(te_md5s_list, 'a+')
-    existing_md5s = md5_list.readlines().sort()
+    existing_md5s = sorted(md5_list.readlines())
 
-    # For pages 1-10 of reports, get MD5s
-    for i in range(1, 11):
-        md5s += get_report_md5s(i)
+    # If no search terms, get all latest reports
+    if len(sys.argv) == 1:
+        # For pages 1-10 of reports, get MD5s
+        for i in range(1, 11):
+            te_reports_url = "http://www.threatexpert.com/reports.aspx?page=%d" % i
+            req = requests.get(te_reports_url, headers=ie11_req_headers)
+            md5s += get_report_md5s(req.text)
+
+    # If search terms provided, retrieve results from search
+    else:
+        for term in sys.argv[1:]:
+            search = term.strip('"')
+            get_te_search_results(search)
 
     # Append new MD5s to MD5s list
     for md5 in md5s:
